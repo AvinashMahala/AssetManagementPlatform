0efaee667e3ab655efbb97744e0f9e4b0f8c4dad
Avinash Mahala <avinash.mahala.am@outlook.com>
2025-12-17 08:25:03 -0800

    refactor: remove MeterCreatePage and update exports

--- frontend/src/pages/meters/index.ts
+++ frontend/src/pages/meters/index.ts
@@ -1,5 +1,4 @@
 export { MeterListPageEnhanced } from './MeterListPageEnhanced';
-export { MeterCreatePage } from './MeterCreatePage';
 export { default as MeterCreatePageTabbedEnhanced } from './MeterCreatePageTabbedEnhanced';
 export { MeterEditPage } from './MeterEditPage';
 export { MeterDetailPage } from './MeterDetailPage';
